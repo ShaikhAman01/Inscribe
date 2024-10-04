7f4a68f97eb5547699c65f1c77dd1381dedce72f
Shaikh Aman <110104595+ShaikhAman01@users.noreply.github.com>
2024-10-04 23:57:32 +0530

    Initialize the schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,29 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.post('/api/v1/signup', (c) => {
-  return c.text('Hello Hono!')
-})
-app.post('/api/v1/signin', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/api/v1/signup", (c) => {
+  return c.text("Signup route");
+});
+app.post("/api/v1/signin", (c) => {
+  return c.text("Signin route");
+});
 
-app.post('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-app.put('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/api/v1/blog", (c) => {
+  return c.text(" Blog route to create");
+});
+app.put("/api/v1/blog", (c) => {
+  return c.text(" Blog route to edit");
+});
 
-app.get('/api/v1/blog/:id', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/api/v1/blog/:id", (c) => {
+  const id = c.req.param("id");
+  console.log(id);
+  return c.text("Get blog route");
+});
 
-app.get('/api/v1/blog/bulkx', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/api/v1/blog/bulkx", (c) => {
+  return c.text("Get all the blogs");
+});
 
-export default app
+export default app;
